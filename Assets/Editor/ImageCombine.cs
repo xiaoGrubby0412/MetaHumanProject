da309e342cb3a2e0a7121994198a9bd679e04058
xiaoGrubby0412 <827882133@qq.com>
2023-04-20 01:29:14 +0800

    commit project

---
+++ Assets/Editor/ImageCombine.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ImageCombine
+{
+    [MenuItem("ImageTool/CombineImage")]
+    public static void CombineImage()
+    {
+        const string folder = @"D:\ºÏ²¢AlphaÍ¼Æ¬";
+ 
+        
+    }
+}
