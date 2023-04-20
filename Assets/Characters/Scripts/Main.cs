43349aa8d36dae8ae3d502ad80c5c7ad466c6c3c
wangman09 <wangman09@baidu.com>
2023-04-20 11:19:52 +0800

    commit robot

---
+++ Assets/Characters/Scripts/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using Baidu.VR.Zion;
+
+public class Main : MonoBehaviour
+{
+    private GameObject playerObj;
+    public Vector3 birthPoint = Vector3.zero;
+    public static Main Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+        playerObj = GameObject.Instantiate(Resources.Load("Player")) as GameObject;
+        playerObj.transform.position = birthPoint;
+        playerObj.transform.localScale = Vector3.one;
+        playerObj.transform.rotation = Quaternion.identity;
+        
+        GameObject.Instantiate(Resources.Load("MainUiPanelCanvas"));
+#if !UNITY_EDITOR
+        #if UNITY_ANDROID || UNITY_IOS
+            GameObject JoyStickCanvas = GameObject.Instantiate(Resources.Load("JoyStick/JoystickCanvas") as GameObject);
+        #endif
+#endif
+    }
+
+    private void OnDestroy()
+    {
+        Instance = null;
+    }
+}
