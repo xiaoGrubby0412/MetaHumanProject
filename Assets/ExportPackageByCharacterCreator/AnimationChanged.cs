5c8d724a593dfee34104f5654b5511bb8f4a6121
xiaoGrubby0412 <827882133@qq.com>
2023-04-24 00:36:26 +0800

    commit animation

--- Assets/ExportPackageByCharacterCreator/AnimationChanged.cs
+++ Assets/ExportPackageByCharacterCreator/AnimationChanged.cs
@@ -7,18 +7,18 @@ public class AnimationChanged : MonoBehaviour
 {
     public enum AnimationState 
     {
-        Idle01 = 1,
-        Run = 2,
-        Sitting01 = 3,
-        Sitting02 = 4,
-        Sitting03 = 5,
-        SitToStand = 6,
-        StandToSitting = 7,
+        Idle = 1,
+        Running = 2,
+        SitToStand = 3,
+        Sitting = 4,
+        SittingIdle = 5,
+        StandToSit = 6,
+        Walk = 7,
     }
 
     string aniStr = "Armature|";
 
-    public AnimationState CurState = AnimationState.Idle01;
+    public AnimationState CurState = AnimationState.Idle;
     public Animator animator;
     // Start is called before the first frame update
     void Start()
@@ -30,6 +30,19 @@ public class AnimationChanged : MonoBehaviour
     private void PlayerAnimationByState(AnimationState state)
     {
         string aniName = aniStr + Enum.GetName(typeof(AnimationState), state);
+        if (state == AnimationState.SitToStand)
+        {
+            aniName = "Armature|Sit To Stand";
+        }
+        else if (state == AnimationState.StandToSit)
+        {
+            aniName = "Armature|Stand To Sit";
+        }
+        else if (state == AnimationState.SittingIdle)
+        {
+            aniName = "Armature|Sitting Idle";
+        }
+
         animator.CrossFadeInFixedTime(aniName, 0.25f);
         CurState = state;
           
@@ -40,31 +53,31 @@ public class AnimationChanged : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Keypad1))
         {
-            PlayerAnimationByState(AnimationState.Idle01);
+            PlayerAnimationByState(AnimationState.Idle);
         }
         else if (Input.GetKeyDown(KeyCode.Keypad2))
         {
-            PlayerAnimationByState(AnimationState.Run);
+            PlayerAnimationByState(AnimationState.Running);
         }
         else if (Input.GetKeyDown(KeyCode.Keypad3))
         {
-            PlayerAnimationByState(AnimationState.Sitting01);
+            PlayerAnimationByState(AnimationState.SitToStand);
         }
         else if (Input.GetKeyDown(KeyCode.Keypad4))
         {
-            PlayerAnimationByState(AnimationState.Sitting02);
+            PlayerAnimationByState(AnimationState.Sitting);
         }
         else if (Input.GetKeyDown(KeyCode.Keypad5))
         {
-            PlayerAnimationByState(AnimationState.Sitting03);
+            PlayerAnimationByState(AnimationState.SittingIdle);
         }
         else if (Input.GetKeyDown(KeyCode.Keypad6))
         {
-            PlayerAnimationByState(AnimationState.SitToStand);
+            PlayerAnimationByState(AnimationState.StandToSit);
         }
         else if (Input.GetKeyDown(KeyCode.Keypad7))
         {
-            PlayerAnimationByState(AnimationState.StandToSitting);
+            PlayerAnimationByState(AnimationState.Walk);
         }
     }
 }
